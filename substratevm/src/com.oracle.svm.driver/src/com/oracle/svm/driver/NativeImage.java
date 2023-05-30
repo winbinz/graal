2dee901d83f9702e0c0397721ea0360fa90360d4
Fabio Niephaus <fabio.niephaus@oracle.com>
2023-05-30 12:04:34 +0200

    Fix progress reporting.

--- substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
+++ substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
@@ -2110,7 +2110,11 @@ public class NativeImage {
          * When DebugOptions.Log is used and no LogFile is set, progress cannot be reported as
          * logging works around NativeImageSystemIOWrappers to access stdio handles.
          */
-        return !getHostedOptionArgumentValues(imageBuilderArgs, oH + "Log=").isEmpty() && !logRedirectedToFile();
+        if (!getHostedOptionArgumentValues(imageBuilderArgs, oH + "Log=").isEmpty()) {
+            return logRedirectedToFile();
+        }
+
+        return true;
     }
 
     private static boolean logRedirectedToFile() {
